e5f7755f14243a811e964ee69ea211fdf43ce386
erwinjames <manugasewinjames@gmail.com>
2023-01-21 18:46:32 +0800

    updating

--- dashboard/admin/resources/config.php
+++ dashboard/admin/resources/config.php
@@ -4,10 +4,10 @@ define("TEMPLATES_PATH", realpath(dirname(__FILE__) . '/templates'));
 define("MAIN_CSS_PATH", realpath(dirname(__FILE__) . './css/default-assets/'));
 
 
-   define("DB_HOST", "server143.web-hosting.com");
-   define("DB_ROOT", "barkicek_barkomatic");
-   define("DB_PASS", "barkomatic@barkomatic");
-   define("DB_NAME", "barkicek_barkomatic");
+define("DB_HOST", "localhost");
+define("DB_ROOT", "root");
+define("DB_PASS", "");
+define("DB_NAME", "barkomatic");
    
     $con = mysqli_connect(DB_HOST, DB_ROOT, DB_PASS, DB_NAME);
     if (!$con) {
