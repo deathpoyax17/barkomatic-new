ea0a55b75c3be201511c1a9953e748349abfdd5b
jayar autentico <deathpoyax17@gmail.com>
2023-02-12 03:28:12 +0800

    updating css

--- resources/templates/_header(withmodifyitinerary).php
+++ resources/templates/_header(withmodifyitinerary).php
@@ -483,7 +483,7 @@
             </div >
         </div -->
      <!-- Header Area Start -->
-     <section class="navigation">
+     <!-- <section class="navigation"> 
       <div class="container">
   <div class="nav-container">
     <div class="brand">
@@ -518,7 +518,38 @@
     </nav>
   </div>
 </div>
-</section>
+</section> -->
+
+<header>
+        <a href="index.html">
+            <img src="img/core-img/barkomatic-logo.png" alt="barkomatic-logo" class="barkomatic-logo">
+        </a>
+        <div class="navigation">
+            <ul class="menu">
+                <div class="close-btn"></div>
+                <li class="menu-item"><a href="#">Home</a></li>
+                <li class="menu-item"><a href="#">Contact Us</a></li>
+                <li class="menu-item"><a href="#">Tracker</a></li>
+                <li class="menu-item"><a href="#">Privacy Policy</a></li>
+                <li class="menu-item">
+                    <a class="sub-btn" href="#">Passenger Guidelines <i class="fas fa-angle-down"></i></a>
+                    <ul class="sub-menu">
+                        <li class="sub-item"><a href="#">Booking/Boarding Guide</a></li>
+                        <li class="sub-item"><a href="#">Rebooking Guidelines</a></li>
+                        <li class="sub-item"><a href="#">Refund Guidelines</a></li>
+                    </ul>
+                </li>
+                <li class="menu-item">
+                    <a class="sub-btn" href="#">Cargo Guidelines <i class="fas fa-angle-down"></i></a>
+                    <ul class="sub-menu">
+                        <li class="sub-item"><a href="#">Book/BoardGuide</a></li>
+                    </ul>
+                </li>
+            </ul>
+        </div>
+        <div class="menu-btn"></div>
+    </header>
+   
 
     <!-- Header Area End -->
 
@@ -561,4 +592,36 @@
             </div>
         </div>
    
-                     
+        <script type="text/javascript">
+        //jquery for toggle dropdown menus
+        $(document).ready(function() {
+            //toggle sub-menus
+            $(".sub-btn").click(function() {
+                $(this).next(".sub-menu").slideToggle();
+            });
+
+            //toggle more-menus
+            $(".more-btn").click(function() {
+                $(this).next(".more-menu").slideToggle();
+            });
+        });
+
+        //javascript for the responsive navigation menu
+        var menu = document.querySelector(".menu");
+        var menuBtn = document.querySelector(".menu-btn");
+        var closeBtn = document.querySelector(".close-btn");
+
+        menuBtn.addEventListener("click", () => {
+            menu.classList.add("active");
+        });
+
+        closeBtn.addEventListener("click", () => {
+            menu.classList.remove("active");
+        });
+
+        //javascript for the navigation bar effects on scroll
+        window.addEventListener("scroll", function() {
+            var header = document.querySelector("header");
+            header.classList.toggle("sticky", window.scrollY > 0);
+        });
+    </script>
