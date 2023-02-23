f9d374ab99bccee0cdbcaea8cdb18ca8be13b434
jayar autentico <deathpoyax17@gmail.com>
2023-02-24 01:33:33 +0800

    updating e-ticket and re-edit searchtrip

---
+++ e-ticket.php
@@ -0,0 +1,120 @@
+<!DOCTYPE html>
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <html lang="en">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="/resources/demos/style.css">
+    <link rel="icon" type="image/png" href="img/core-img/favicon.png">
+    <link rel="stylesheet" href="assets/vendors/bootstrap/css/bootstrap.min.css" media="all">
+    <link rel="stylesheet" type="text/css" href="assets/vendors/fontawesome/css/all.min.css">
+    <link rel="stylesheet" type="text/css" href="assets/vendors/jquery-ui/jquery-ui.min.css">
+    <link rel="stylesheet" type="text/css" href="assets/vendors/modal-video/modal-video.min.css">
+    <link rel="stylesheet" type="text/css" href="assets/vendors/lightbox/dist/css/lightbox.min.css">
+    <link rel="stylesheet" type="text/css" href="assets/vendors/slick/slick.css">
+    <link rel="stylesheet" type="text/css" href="assets/vendors/slick/slick-theme.css">
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,600;0,700;0,800;1,400&family=Raleway:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,400&display=swap" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Libre+Barcode+39+Text&display=swap" rel="stylesheet">
+    <script src="/assets/js/calendar-script.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js"></script>
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.7.0/moment-with-langs.min.js"></script>
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jqueryui-touch-punch/0.2.3/jquery.ui.touch-punch.min.js"></script>
+
+    <title>Barkomatic - Online Ticketing</title>
+</head>
+
+<body class="container-background-eticket">
+    <div class="container-eticket">
+
+        <div class="header-container">
+            <h2>Ship Ticket</h2>
+            <span class="logo-shippinglines">
+                <img src="assets/images/vgshipping.png" alt="vgshipping-logo" style="max-width: 80px; max-height: 80px;">
+            </span>
+            <div class="eticket-shipdetails">
+                <div>
+                    <span style="font-weight: bold;">VG Shipping Lines</span>
+                </div>
+                <div>
+                    <span style="font-weight: bold;">Address:</span>
+                    <span> 7WX5+7QR, V. Sotto St, Pier 3, Cebu City, Cebu</span>
+                </div>
+                <div>
+                    <span style="font-weight: bold;">Phone:</span>
+                    <span> (032) 384 7570</span>
+                </div>
+            </div>
+        </div>
+        <div class="header-eticket">
+
+        </div>
+
+        <div class="body-eticket">
+            <div class="first-linedetails">
+                <p><span style="font-weight: bold;">Name:</span>
+                    <span> Jay-ar T Autentico</span>
+                </p>
+                <p><span style="font-weight: bold;">FROM:</span>
+                    <span> Cebu</span>
+                </p>
+                <p><span style="font-weight: bold;">VESSEL:</span>
+                    <span> VG-RORO</span>
+                </p>
+                <p><span style="font-weight: bold;">PORT:</span>
+                    <span> Pier 3</span>
+                </p>
+            </div>
+            <div class="second-linedetails">
+                <p><span style="font-weight: bold;">TO:</span>
+                    <span> Talibon, Bohol</span>
+                </p>
+                <p><span style="font-weight: bold;">DATE:</span>
+                    <span> FEB 26, 2023</span>
+                </p>
+                <p><span style="font-weight: bold;">DEPARTURE TIME:</span>
+                    <span> 10:00PM</span>
+                </p>
+            </div>
+            <div class="third-linedetails">
+                <p><span style="font-weight: bold;">SEAT:</span>
+                    <span>Aircon-1A</span>
+                </p>
+            </div>
+            <div class="barcode-eticket" style="font-family: 'Libre Barcode 39 Text', cursive;">
+                <span>1234567890</span>
+                <h1>barcode</h1>
+            </div>
+        </div>
+        <div class="footer-container">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-8 d-none d-lg-block">
+                        <div class="footer-contact-info">
+                            <ul>
+                                <li>
+                                    <a href="#"><i class="fas fa-phone-alt"></i> +64 (909) 1234 396</a>
+                                </li>
+                                <li>
+                                    <a href="mailto:info@Travel.com"><i class="fas fa-envelope"></i> BarkoamticOnlineTicketing@gmail.com</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
