d349dab200e2a558ecfefb171047a88e4abe9501
manugasewinjames@gmail.com <manugasewinjames@gmail.com>
2023-02-27 17:04:18 +0800

    update

--- resources/config.php
+++ resources/config.php
@@ -3,9 +3,9 @@
     define("MAIN_CSS_PATH", realpath(dirname(__FILE__) . './css/default-assets/'));
     
    define("DB_HOST", "localhost");
-   define("DB_ROOT", "root");
-   define("DB_PASS", "");
-   define("DB_NAME", "barkomatic-new");
+   define("DB_ROOT", "u846678508_barkomatic");
+   define("DB_PASS", "Deathpoyax9876");
+   define("DB_NAME", "u846678508_barkomatic");
    
     $con = mysqli_connect(DB_HOST, DB_ROOT, DB_PASS, DB_NAME);
     if (!$con) {
