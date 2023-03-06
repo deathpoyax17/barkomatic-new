6d0760e87f57a674670900bab70be01848a52a30
manugasewinjames@gmail.com <manugasewinjames@gmail.com>
2023-03-06 13:10:25 +0800

    test

--- subscription/paypal_ipn.php
+++ subscription/paypal_ipn.php
@@ -128,9 +128,9 @@ if (strcmp($res, "VERIFIED") == 0 || strcasecmp($res, "VERIFIED") == 0) {
                 $insert = $con->query($sql); 
                  
                 // Update subscription id in the users table 
-                if($insert && !empty($custom)){ 
+                if($insert){ 
                     $subscription_id = $con->insert_id; 
-                    $update = $con->query("UPDATE ship_owners SET subscription_id = $subscription_id WHERE owner_id = $custom"); 
+                    $update = $con->query("UPDATE ship_owners SET subscription_id=17 WHERE owner_id=1"); 
                 } 
             } 
         } 
