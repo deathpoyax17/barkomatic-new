7c76134f8dd4f007a96deeabba3f329cffbdaa00
manugasewinjames@gmail.com <manugasewinjames@gmail.com>
2023-03-08 14:21:14 +0800

    test

--- modules/purchaseAvail/notify.php
+++ modules/purchaseAvail/notify.php
@@ -269,52 +269,51 @@ try {
 					   if($insert && !empty($custom)){ 
 						//    $subscription_id = $con->insert_id; 
 						//    $update = $con->query("UPDATE ship_owners SET subscription_id = {$subscription_id} WHERE id = {$custom}"); 
-						try {
-							$mail = new PHPMailer();
-							// $mail->SMTPDebug = SMTP::DEBUG_SERVER;
-							// $mail->SMTPDebug = 3;
-							$mail->isSMTP();
-							$mail->SMTPAuth = false;
-							$mail->SMTPAutoTLS = false;
-							$mail->Host = "localhost";
-							$mail->Username = 'barkomatic@barkomatic.xyz';
-							$mail->Password = 'barkomatic@barkomatic';
-							$mail->Port = 25;
-							$mail->setFrom('barkomatic@barkomatic.xyz', 'Reservation');
-							$mail->addAddress($_SESSION['email']);
-							$mail->isHTML(true);
-							$mail->Subject = 'Reservation Confirmation';
-							$mail->Body = "
-							<!DOCTYPE html>
-							<head>
-							<style>
-								body {
-									font-family: Cambria, Cochin, Georgia, Times, 'Times New Roman', serif;
-									}
-							</style>
-							</head>
-							<body>
-								<div class='container m-auto'>
-									<div class='row'>
-										<div class='col-sm-12'>
-											<h4>$ship_name</h4><br>
-											<p>Hello $pssngr_fname, Thank you for making your reservation in our shipping line. <br>Your <b>Payment</b> will be handled in the ticket office.</p>
-											<p>Your ticket reservation is valid until: <b>$exp</b></p>
-											<p>If you find it necessary to cancel or change plans, please inform us by email <span style='color:#007bff;font-weight:700;'>$ship_email<span></p>
-											<br><br>
-											<p>Again, thank you for choosing us. We look forward to having you as our guest.</p>
-											<p>Best regards,<br><span>Reservation Office</span></p>
+							try {
+								$mail = new PHPMailer();
+								// $mail->SMTPDebug = SMTP::DEBUG_SERVER;
+								// $mail->SMTPDebug = 3;
+								$mail->isSMTP();
+								$mail->SMTPAuth = true;
+								$mail->SMTPAutoTLS = false;
+								$mail->Host = "smtp.hostinger.com";
+								$mail->Username = 'support@barkomatic.online';
+								$mail->Password = 'Deathpoyax@9876';
+								$mail->Port = 587; // or 465
+								$mail->setFrom('support@barkomatic.online', 'Confirmation');
+								$mail->addAddress("manugasewinjames@gmail.com");
+								$mail->isHTML(true);
+								$mail->Subject = 'Confirmation';
+								$mail->Body = "
+								<!DOCTYPE html>
+								<head>
+								<style>
+									body {
+										font-family: Cambria, Cochin, Georgia, Times, 'Times New Roman', serif;
+										}
+								</style>
+								</head>
+								<body>
+									<div class='container m-auto'>
+										<div class='row'>
+											<div class='col-sm-12'>
+												<h4></h4><br>
+												<p>Hello Thank you for making your reservation in our shipping line. <br>Your <b>Payment</b> will be handled in the ticket office.</p>
+												<p>Your ticket reservation is valid until: <b></b></p>
+												<p>If you find it necessary to cancel or change plans, please inform us by email <span style='color:#007bff;font-weight:700;'><span></p>
+												<br><br>
+												<p>Again, thank you for choosing us. We look forward to having you as our guest.</p>
+												<p>Best regards,<br><span>Reservation Office</span></p>
+											</div>
 										</div>
 									</div>
-								</div>
-							</body>
-							</html>";
-							$mail->send();
-							echo "Your reservation is submitted, In a while you will recieve an email confirmation for your reservation.";
-						}catch(Exception $e){
-							echo "Could not sent the reservation confirmation. Mailer Error: {$mail->ErrorInfo}";
-							// echo 'Could not sent the reservation confirmation.{$mail->ErrorInfo}';
-						}
+								</body>
+								</html>";
+								$mail->send();
+								echo "Your reservation is submitted, In a while you will recieve an email confirmation for your reservation.";
+							}catch(Exception $e){
+								echo "Could not sent the reservation confirmation. Mailer Error: {$mail->ErrorInfo}";
+							}
 					   } 
 				   } 
 			   } 
